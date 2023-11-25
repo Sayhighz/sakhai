2faa7a7c19235867b244d6ef24ce928df40be781
Pratan Nilson <119069194+Sayhighz@users.noreply.github.com>
2023-11-25 20:19:55 +0700

    update navbar

--- javascript/search.js
+++ javascript/search.js
@@ -52,3 +52,14 @@ let search = () => {
 //         }
 //     }
 // }
+
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.opacity = "0.7";
+    } else {
+        document.getElementById("navbar").style.opacity = "1";
+    }
+}
